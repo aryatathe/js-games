e1772145f3d96d33070acbc49649ee592cfad25c
aryatathe <aryatathe@gmail.com>
2023-12-15 10:39:36 +0530

    error test

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { gameList } from "./_configs/game-list";
 
 import Home from "./_Home";
+import Error from "./_Error";
 
 const router = createBrowserRouter(
   [
     {
       path: "/",
       element: <Home />,
+      errorElement: <Error />,
     },
     ...gameList,
   ],
